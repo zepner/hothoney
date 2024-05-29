7a17ae9724d4dc5777d4315f6e6ffa98b20a7f69
Zvi Epner <zepner@gmail.com>
2024-05-29 04:53:27 -0600

    responsive, css linked file

--- index.php
+++ index.php
@@ -1,270 +1,10 @@
 <html>
-
   <head>
+    <meta content="width=device-width, initial-scale=1" name="viewport" />
     <link rel="stylesheet" id="google-fonts-css"
       href="https://fonts.googleapis.com/css?family=Cormorant:300,300i,400,400i,500,600,600i,700|Lato:300,300i,400,400i,500,600,600i,700&subset=latin-ext&display=swap&ver=1.0.0"
       type="text/css" media="all">
-    <style>
-      body {
-        background-color: #4f2024;
-        background-repeat: no-repeat;
-        background-size: cover;
-        background: url(assets/home-9-background-2a.jpg);
-        font-family: Lato, sans-serif;
-        font-size: 18px;
-        color: #fff;
-        padding: 0;
-        margin: 0;
-        -webkit-font-smoothing: antialiased;
-      }
-      #header {
-        position: absolute;
-        display: flex;
-        justify-content: space-between;
-        width: 95%;
-        background: transparent;
-        padding: 1rem;
-        top: 0;
-        left: 1%;
-        align-items: center;
-      }
-      #header #hb-menu {
-        width: 50px;
-        height: 10px;
-        overflow: hidden;
-        border: 1px solid #fff;
-        border-width: 1px 0;
-      }
-      #header #hb-menu a {
-        visibility: hidden;
-      }
-
-      main {
-        width: 100%;
-      }
-      main .block {
-        float: none;
-        display: block;
-        align-items: center;
-        justify-content: center;
-        box-sizing: content-box;
-      }
-      main .block.flex {
-        display: flex;
-        justify-content: space-evenly;
-      }
-      #hero #social {
-        line-height: 2;
-        padding-left: 2rem;
-        max-width: 100%;
-      }
-      #hero #social a {
-        display: block;
-        font-size: 12px;
-        font-weight: 700;
-        letter-spacing: 3.2px;
-        width: 100px;
-      }
-      #hero .copy h1 {
-        font-family: Cormorant, serif;
-        font-size: 6rem;
-        font-weight: 300;
-        line-height: 1;
-        margin-top: 0;
-        margin-bottom: 4rem;
-      }
-      #hero .copy h2 {
-        font-size: 0.75rem;
-        font-weight: 400;
-        line-height: 1;
-        text-transform: uppercase;
-        letter-spacing: 1px;
-      }
-      #hero .copy p {
-        font-size: 1.4rem;
-        line-height: 1.6;
-      }
-      #hero .copy button {
-        border: 2px solid #fff;
-        margin-top: 2rem;
-
-        background: transparent;
-        padding: 11px 55px;
-        letter-spacing: 2.4px;
-        color: #fff;
-        /* border: 1px solid #fff; */
-        text-transform: uppercase;
-        font-size: 65%;
-        font-weight: 700;
-        font-family: Lato, sans-serif;
-      }
-      #hero .copy button a {
-        color: inherit;
-        text-decoration: none;
-      }
-      #logo {
-        font-size: 12px;
-        border: 1px solid rgb(255 255 255 / 50%);
-      }
-      #logo #lh1, #logo #lh2, #logo .invisible {
-        font-family: Cormorant, Georgia, 'Times New Roman', Times, serif;
-        font-size: 6rem;
-        font-weight: 300;
-      }
-      #logo .invisible {
-        font-size: 6.1rem;
-        line-height: 1;
-      }
-      #logo #lh1, #logo #lh2 {
-        line-height: 0;
-      }
-      #logo #lh1 {
-        opacity: .75;
-      }
-      #logo #hhoney {
-        position: absolute;
-        top: 95px;
-        left: 22px;
-        line-height: 0;
-        font-size: 0.5rem;
-        font-weight: bold;
-        letter-spacing: 2px;
-        text-transform: uppercase;
-        /* color: gold; */
-      }
-      .invisible {
-        visibility: hidden;
-      }
-      .italic {
-        font-style: italic;
-      }
-      .bold {
-        font-weight: bold;
-      }
-      .uppercase{
-        text-transform: uppercase;
-      }
-      .block.bginfo {
-        text-align: center;
-        padding: 0 20%;
-      }
-      .block.bginfo h3 {
-        font-family: Cormorant, serif;
-        font-size: 5rem;
-        font-weight: 300;
-        line-height: 1;
-        margin-bottom: 2rem;
-      }
-      .block.bginfo h4 {
-        font-size: 12px;
-        font-weight: 700;
-        letter-spacing: 3.2px;
-      }
-      .block.bginfo p {
-        margin-top: 3rem;
-        line-height: 1.5em;
-        font-weight: 400;
-      }
-      .block#gallery {
-        padding: 4rem 1rem;
-      }
-      #gallery .grid {
-        max-width: 45%;
-        margin: 0 auto;
-        display: flex;
-        flex-flow: row wrap;
-        justify-content: center;
-      }
-
-      #gallery .grid .cell {
-        margin: 1.2rem;
-      /*   background: lightyellow; */
-        flex: 0 1 20%;
-        text-align: center;
-      }
-      #gallery .grid .cell img {
-        max-width: 100%;
-      }
-
-      #products {
-        display: flex;
-        justify-content: space-between;
-        max-width: 80%;
-        margin: auto;
-        padding: 2rem 0 4rem 0;
-      }
-      #products .product {
-        text-align: center;
-        width: 30%;
-        padding: 70px 30px 87px;
-        border: 1px solid #907b7c;
-        margin: 0 1rem;
-      }
-      #products .product h3 {
-        font-family: Cormorant, Georgia, 'Times New Roman', Times, serif;
-        font-size: 3rem;
-        font-weight: 300;
-        font-style: italic;
-        margin-top: 0;
-        margin-bottom: 0;
-      }
-      #products .product .price {
-        font-family: Cormorant, Georgia, 'Times New Roman', Times, serif;
-        font-size: 4rem;
-        font-weight: 300;
-        margin-top: 0;
-        margin-bottom: 1rem;
-      }
-      #products .product .content ul {
-        list-style: none;
-        line-height: 2;
-        margin: 0;
-        padding: 0;
-      }
-      #products .product button {
-        margin-top: 2rem;
-        background: transparent;
-        padding: 11px 55px;
-        letter-spacing: 2.4px;
-        color: #fff;
-        border: 1px solid #fff;
-        text-transform: uppercase;
-        font-size: 65%;
-        font-weight: 700;
-        font-family: Lato, sans-serif;
-      }
-      #products .product button.filled {
-        background-color: #fff;
-        color: #663130;
-      }
-
-      #socpruf {
-        padding-bottom: 4rem;
-      }
-
-      /* Footer */
-      footer {
-        padding: 2rem;
-      }
-      footer #menu {
-        display: flex;
-        justify-content: space-between;
-        width: 100%;
-        margin: auto;
-        align-items: center;
-      }
-      footer #menu h6 {
-        font-family: Cormorant, serif;
-        font-weight: 300;
-        font-size: 1.6rem;
-        line-height: 1;
-        margin: 0;
-      }
-      footer #menu a {
-        color: inherit;
-        text-decoration: none;
-      }
-    </style>
+    <link rel="stylesheet" href="hhstyle.css">
     <!-- MailerLite Universal -->
     <script>
       (function (w, d, e, u, f, l, n) {
@@ -281,7 +21,7 @@
   </head>
   <body>
     <div id="header">
-      <div id="logo" style="position: relative; width: 108px;">
+      <div id="logo">
         <div style="padding: 1rem 1rem 0.5rem 1rem">
           <div id="lh1" style="position: absolute; top: 50px; left: 20px;">H</div>
           <div id="lh2" style="position: absolute; top: 50px; left: 15px;">H</div>
@@ -289,20 +29,20 @@
         </div>
         <div id="hhoney">Hot Honey</div>
       </div>
-      <div class="spacer">&nbsp;</div>
-      <div id="hb-menu"><a>Hamburger</a>></div>
+      <!-- <div class="spacer">&nbsp;</div>
+      <div id="hb-menu"><a>Hamburger</a>></div> -->
     </div>
     <main>
-      <div id="hero" class="block flex" style="padding: 8rem 3rem 3rem; background-image: url(assets/home-9-image-1a.jpg); background-repeat: no-repeat; background-size: cover;">
+      <div id="hero" class="block">
         <div id="social" style=" width: 10%;">
           <a id="instagram">IG</a>
           <a id="facebook">FB</a>
           <a id="linkedin">LI</a>
         </div>
-        <div id="main-image" style="margin: 0 2rem; width: 42%;">
+        <div id="main-image">
           <img src="assets/hero-main-img.png" width="600" style="max-width: 90%;" />
         </div>
-        <div class="copy" style="width: 42%">
+        <div class="copy">
           <h2>Build your list, fill your book</h2>
           <h1>Established Beauty Pro</h1>
           <p class="italic bold">Without a list, you're at the mercy of the market.</p>
@@ -315,59 +55,59 @@
         <h4 class="uppercase">A FULL RANGE OF SERVICES</h4>
         <p class="italic" style="font-size: larger">Get serious about email list development with features that include <strong>automated messaging</strong>, <strong>subscriber segmentation</strong>, <strong>evergreen campaigns</strong>, <strong>landing pages</strong>, <strong>scheduling</strong>, and more. <br>As you grow, your system with grow with you - and it's portable! So you can take your clients with you, wherever you go!</p>
       </div>
-      <div id="gallery" class="block" style="display: flex;">
+      <div id="gallery" class="block">
         <div class="left grid">
           <div class="cell"><img src="assets/h9-img-4.jpg"/></div>
           <div class="cell"><img src="assets/h9-img-5.jpg" /></div>
           <div class="cell"><img src="assets/h9-img-6.jpg" /></div>
-          <div class="cell"><img src="assets/h9-img-7.jpg" /></div>
-          <div class="cell"><img src="assets/h9-img-8.jpg" /></div>
-          <div class="cell"><img src="assets/h9-img-9.jpg" /></div>
-        </div>
-        <div class="right onebig" style="overflow: auto;">
-          <img width="600" height="542"
-            src="assets/h9-img-03-600x542.jpg">
         </div>
       </div>
       <div id="products" class="block">
         <div id="standard" class="product">
           <h3>Standard</h3>
-          <div class="price">$89</div>
+          <div class="price">$349</div>
           <div class="content">
             <ul>
-              <li>Lorem ipsum dolor sit amet</li>
-              <li>Consectetur facit duo</li>
-              <li>Dolore amet loric incid</li>
-              <li>Dexu devi demo</li>
+              <li>Basic Branding</li>
+              <li>Mailchimp Integration</li>
+              <li>Landing Page Creation</li>
+              <li>Subscription Option</li>
+              <li>Subscriber Email Series</li>
             </ul>
           </div>
-          <button>Purchase</button>
+          <button><a href="#contact">Purchase</a></button>
         </div>
         <div id="premium" class="product" style="background: url(assets/h9-img-10.jpg); background-repeat: no-repeat; background-size: cover; background-position-x: right;">
           <h3>Premium</h3>
-          <div class="price">$119</div>
+          <div class="price">$749</div>
           <div class="content">
             <ul>
-              <li>Lorem ipsum dolor sit amet</li>
-              <li>Consectetur facit duo</li>
-              <li>Dolore amet loric incid</li>
-              <li>Dexu devi demo</li>
+              <li>Branding</li>
+              <li>Mailchimp Integration</li>
+              <li>Website Services, Contact</li>
+              <li>Subscription Sequence</li>
+              <li>Premium Design</li>
+              <li>Customized User Journey</li>
+              <li>1 Social Media Ad (Image + Copy)</li>
             </ul>
           </div>
-          <button class="filled">Purchase</button>
+          <button><a href="#contact">Purchase</a></button>
         </div>
         <div id="deluxe" class="product">
           <h3>Deluxe</h3>
-          <div class="price">$149</div>
+          <div class="price">$1249</div>
           <div class="content">
             <ul>
-              <li>Lorem ipsum dolor sit amet</li>
-              <li>Consectetur facit duo</li>
-              <li>Dolore amet loric incid</li>
-              <li>Dexu devi demo</li>
+              <li>Branding</li>
+              <li>Mailchimp Integration</li>
+              <li>Website Services, Contact</li>
+              <li>Subscription Sequence</li>
+              <li>Premium Design</li>
+              <li>Customized User Journey</li>
+              <li>2 Social Media Ads (Image + Copy)</li>
             </ul>
           </div>
-          <button>Purchase</button>
+          <button><a href="#contact">Speak to a Rep</a></button>
         </div>
       </div>
       <div id="socpruf" class="block bginfo">
@@ -397,7 +137,7 @@
           <h6>Email</h6>
           <p><a href="mailto:summer@hothoney.studio">summer@hothoney.studio</a></p>
         </div>
-        <div id="logo" style="position: relative; width: 108px;">
+        <div id="logo">
           <div style="padding: 1rem 1rem 0.5rem 1rem;">
             <div id="lh1" style="position: absolute; top: 50px; left: 20px;">H</div>
             <div id="lh2" style="position: absolute; top: 50px; left: 15px;">H</div>
